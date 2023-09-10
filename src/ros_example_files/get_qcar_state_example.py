22ff6e4c21456cf657e253afe672d5c95aa2f0ee
Jacob <jacob.davison@uon.edu.au>
2023-09-10 22:16:35 +1000

    Updated launch files with comments, added more example files with detailed commenting

--- src/ros_example_files/get_qcar_state_example.py
+++ src/ros_example_files/get_qcar_state_example.py
@@ -4,43 +4,46 @@ from gazebo_msgs.srv import GetLinkState
 import math
 import rospy
 
-def quat_to_deg(x, y, z, w):
+# converts the quaterions provided by gazebo into radians
+def quat_to_rad(x, y, z, w):
     a = 2.0 * (w * z + x * y)
     b = 1.0 -2.0 * (y * y + z * z)
     yaw = math.atan2(a, b)
     return yaw
-
+# returns current steering angle of the qcar
 def get_steering_angle():
 	try:
-		
+		# creates a callable proxy to the get link state gazebo service
 		link_state_get = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+		# calls the previously setup proxy to retreive the state of the...
+		#  hubfr link with respect to the base_footprint
 		link_state = link_state_get("qcar::hubfr", "base_footprint")
-		current_steering_angle = (quat_to_deg(link_state.link_state.pose.orientation.x,link_state.link_state.pose.orientation.y,link_state.link_state.pose.orientation.z,link_state.link_state.pose.orientation.w))
+		# inputs the quaterion values provided by gazebo to return a radian steering angle
+		current_steering_angle = (quat_to_rad(link_state.link_state.pose.orientation.x,link_state.link_state.pose.orientation.y,link_state.link_state.pose.orientation.z,link_state.link_state.pose.orientation.w))
 		return current_steering_angle
 
 	except rospy.ServiceException as e:
 		rospy.loginfo("Get Model State service call failed:  {0}".format(e))
 
-
+# returns the state of the qcar in the gazebo simulation
 def get_qcar_state():
-    try:
-    	
-    	qcar_state_get = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-    	qcar_state = qcar_state_get("qcar", "")
+	try:
+		qcar_state_get = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+		qcar_state = qcar_state_get("qcar", "")
+		# extracts the relevant data from the qcar state topic
+		current_x = qcar_state.pose.position.x
+		current_y = qcar_state.pose.position.y
+		current_yaw = quat_to_rad(qcar_state.pose.orientation.x, qcar_state.pose.orientation.y, qcar_state.pose.orientation.z, qcar_state.pose.orientation.w)
+		current_vx = qcar_state.twist.linear.x
+		current_vy = qcar_state.twist.linear.y
+		return current_x, current_y, current_yaw, current_vx, current_vy
+	
+	except rospy.ServiceException as e:
+		rospy.loginfo("Get Model State service call failed:  {0}".format(e))
 
-    	current_x = qcar_state.pose.position.x
-    	current_y = qcar_state.pose.position.y
-    	current_yaw = quat_to_deg(qcar_state.pose.orientation.x, qcar_state.pose.orientation.y, qcar_state.pose.orientation.z, qcar_state.pose.orientation.w)
-    	current_vx = qcar_state.twist.linear.x
-    	current_vy = qcar_state.twist.linear.y
-
-    	return current_x, current_y, current_yaw, current_vx, current_vy
-
-    except rospy.ServiceException as e:
-    	rospy.loginfo("Get Model State service call failed:  {0}".format(e))
-
+# updates the state variables
 def update_state():
-
+	
 	current_x, current_y, current_yaw, current_vx, current_vy = get_qcar_state()
 	current_steering_angle = get_steering_angle()
 
